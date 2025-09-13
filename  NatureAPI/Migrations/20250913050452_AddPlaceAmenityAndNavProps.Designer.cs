95f99aacc6ea04d27dc98e61a152ac52cb316954
unknown <orlandoabta@gmail.com>
2025-09-12 23:15:32 -0600

    PrototipoFinal

---  NatureAPI/Migrations/20250913050452_AddPlaceAmenityAndNavProps.Designer.cs
+++  NatureAPI/Migrations/20250913050452_AddPlaceAmenityAndNavProps.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NatureAPI.Data;
 
@@ -11,9 +12,11 @@ using NatureAPI.Data;
 namespace NatureAPI.Migrations
 {
     [DbContext(typeof(NatureDbContext))]
-    partial class NatureDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250913050452_AddPlaceAmenityAndNavProps")]
+    partial class AddPlaceAmenityAndNavProps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++  NatureAPI/Migrations/20250913050452_AddPlaceAmenityAndNavProps.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NatureAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlaceAmenityAndNavProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: "https://i0.wp.com/www.turimexico.com/wp-content/uploads/2015/07/eehierveelagua.jpg?w=747&ssl=1");
+
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: "https://i0.wp.com/www.turimexico.com/wp-content/uploads/2015/07/eetamul.jpg?w=500&ssl=1");
+
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: "https://www.gob.mx/cms/uploads/article/main_image/27513/blog_izta_popo.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Url",
+                value: "https://upload.wikimedia.org/wikipedia/commons/1/12/Hierve_el_Agua_01.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Url",
+                value: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Cascada_Tamul.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Photos",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Url",
+                value: "https://upload.wikimedia.org/wikipedia/commons/7/74/Iztaccihuatl.jpg");
+        }
+    }
+}
